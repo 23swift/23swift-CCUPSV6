0b229216dfd5d196423bc0b78486bd6a0c061152
23swift <abcostamero@yahoo.com>
2020-07-07 16:31:35 +0800

    fixed field names

--- src/main/java/com/bdo/ccups/model/ProductByInstitution.java
+++ src/main/java/com/bdo/ccups/model/ProductByInstitution.java
@@ -3,11 +3,11 @@ package com.bdo.ccups.model;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
-@Projection(name = "byInstitution", types = {Product.class})
+// @Projection(name = "forDropDown", types = {Product.class})
 public interface ProductByInstitution {
   
-    // @Value("#{target.getInstitution().getId()}")
-    // Long getInstitutionId();
+    @Value("#{target.getInstitution().getName()+' - '+target.getName() }")
+    String getProductName();
    
 
 }
